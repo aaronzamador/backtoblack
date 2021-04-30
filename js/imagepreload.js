46e27e6fb859d0394dc514e4214fcd493b8fe56b
aaronzamador <70031635+aaronzamador@users.noreply.github.com>
2021-04-29 21:25:09 -0500

    [rpelaod?

---
+++ js/imagepreload.js
@@ -0,0 +1,15 @@
+
+			var images = new Array()
+			function preload() {
+				for (i = 0; i < preload.arguments.length; i++) {
+					images[i] = new Image()
+					images[i].src = preload.arguments[i]
+				}
+			}
+			preload(
+				"images/freelunchhifiwhole.jpg",
+				"images/freelunch-tilted.jpg",
+				"images/edhifiwhole.jpg",
+				"images/ed-case.jpg",
+				"images/see-thru.jpg",
+			)
