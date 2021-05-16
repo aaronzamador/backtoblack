5101061016cb70e52c450034612995869c882417
aaronzamador <70031635+aaronzamador@users.noreply.github.com>
2021-05-16 13:32:06 -0500

    s

--- js/aboutmenu.js
+++ js/aboutmenu.js
@@ -87,4 +87,79 @@ $("#footabout").click(function(){
   });
   
 
+  $('.reviewimage1').click(function(e){
+    $('.reviewimage1 img').toggleClass('fullscreen'); 
+});
+
+$('.testingimage').click(function(e){
+    $('.testingimage img').toggleClass('fullscreen'); 
+  });
+
+  $('.compimage1').click(function(e){
+    $('.compimage1 img').toggleClass('fullscreen'); 
+  });
   
+  $('.iap1').click(function(e){
+    $('.iap1 img').toggleClass('fullscreen'); 
+  });
+  
+  $('.flowpic1').click(function(e){
+    $('.flowpic1 img').toggleClass('fullscreen'); 
+  });
+  $('.jp1').click(function(e){
+      $('.jp1 img').toggleClass('fullscreen'); 
+    });
+    $('.sp1').click(function(e){
+        $('.sp1 img').toggleClass('fullscreen'); 
+      });
+      $('.sp2').click(function(e){
+          $('.sp2 img').toggleClass('fullscreen'); 
+        });
+        $('.sp3').click(function(e){
+            $('.sp3 img').toggleClass('fullscreen'); 
+          });
+          $('.sp4').click(function(e){
+              $('.sp4 img').toggleClass('fullscreen'); 
+            });
+            $('.sp5').click(function(e){
+                $('.sp5 img').toggleClass('fullscreen'); 
+              });
+              $('.sp6').click(function(e){
+                  $('.sp6 img').toggleClass('fullscreen'); 
+                });
+
+$('.wireimg').click(function(e){
+$('.wireimg img').toggleClass('fullscreen'); 
+});
+
+
+$('.wireimg2').click(function(e){
+    $('.wireimg2 img').toggleClass('fullscreen'); 
+    });
+
+    
+$('.testp1').click(function(e){
+    $('.testp1 img').toggleClass('fullscreen'); 
+    });
+
+    
+$('.ms1').click(function(e){
+    $('.ms1 img').toggleClass('fullscreen'); 
+    });
+
+    
+$('.ms2').click(function(e){
+    $('.ms2 img').toggleClass('fullscreen'); 
+    });
+
+    
+$('.ms3').click(function(e){
+    $('.ms3 img').toggleClass('fullscreen'); 
+    });
+
+    
+$('.ms4').click(function(e){
+    $('.ms4 img').toggleClass('fullscreen'); 
+    });
+
+    
