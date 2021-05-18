ba33effb754e695209e14d447566e484fa6b1fcb
aaronzamador <70031635+aaronzamador@users.noreply.github.com>
2021-05-17 21:40:16 -0500

    ,

---
+++ js/flshadowbox.js
@@ -0,0 +1,200 @@
+
+  // Open the Modal
+  function openMod() {
+    document.getElementById("myMod").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod() {
+    document.getElementById("myMod").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod2() {
+    document.getElementById("myMod2").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod2() {
+    document.getElementById("myMod2").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod3() {
+    document.getElementById("myMod3").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod3() {
+    document.getElementById("myMod3").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod4() {
+    document.getElementById("myMod4").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod4() {
+    document.getElementById("myMod4").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod5() {
+    document.getElementById("myMod5").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod5() {
+    document.getElementById("myMod5").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod6() {
+    document.getElementById("myMod6").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod6() {
+    document.getElementById("myMod6").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod7() {
+    document.getElementById("myMod7").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod7() {
+    document.getElementById("myMod7").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod8() {
+    document.getElementById("myMod8").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod8() {
+    document.getElementById("myMod8").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod9() {
+    document.getElementById("myMod9").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod9() {
+    document.getElementById("myMod9").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod10() {
+    document.getElementById("myMod10").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod10() {
+    document.getElementById("myMod10").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod11() {
+  document.getElementById("myMod11").style.display = "block";
+}
+
+// Close the Modal
+function closeMod11() {
+  document.getElementById("myMod11").style.display = "none";
+}
+
+  // Open the Modal
+  function openMod12() {
+    document.getElementById("myMod12").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod12() {
+    document.getElementById("myMod12").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod13() {
+    document.getElementById("myMod13").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod13() {
+    document.getElementById("myMod13").style.display = "none";
+  }
+
+  // Open the Modal
+  function openMod14() {
+    document.getElementById("myMod14").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod14() {
+    document.getElementById("myMod14").style.display = "none";
+  }
+    
+  // Open the Modal
+function openMod15() {
+    document.getElementById("myMod15").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod15() {
+    document.getElementById("myMod15").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod16() {
+    document.getElementById("myMod16").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod16() {
+    document.getElementById("myMod16").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod17() {
+    document.getElementById("myMod17").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod17() {
+    document.getElementById("myMod17").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod18() {
+    document.getElementById("myMod18").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod18() {
+    document.getElementById("myMod18").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod19() {
+    document.getElementById("myMod19").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod19() {
+    document.getElementById("myMod19").style.display = "none";
+  }
+  
+  // Open the Modal
+function openMod20() {
+    document.getElementById("myMod20").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMod20() {
+    document.getElementById("myMod20").style.display = "none";
+  }
